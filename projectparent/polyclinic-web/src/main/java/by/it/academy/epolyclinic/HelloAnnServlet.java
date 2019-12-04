4bcc1e4c33a09005b4aecd4db50b2c3dc66ec790
Alisa Stolbunova <alisa.stolbunova@gmail.com>
2019-12-04 17:30:13 +0300

    Servlet

---
+++ projectparent/polyclinic-web/src/main/java/by/it/academy/epolyclinic/HelloAnnServlet.java
@@ -0,0 +1,20 @@
+package by.it.academy.epolyclinic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloAppServlet", urlPatterns = "/sayhello1")
+public class HelloAnnServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>It is another servlet.</h2>");
+    }
+}
